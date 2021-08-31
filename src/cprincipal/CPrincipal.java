e34b59e56a9a3ddac9c3bd0d966cc9e70c6f0bbd
Deyvison Fernandes <deyvisonotaviano@gmail.com>
2021-08-31 00:02:22 -0300

    Criacao do Menu

--- src/cprincipal/CPrincipal.java
+++ src/cprincipal/CPrincipal.java
@@ -1,5 +1,10 @@
 package cprincipal;
 
 public class CPrincipal {
+	public static void main (String[] args) {
+		System.out.println ("=======Menu de Interacao=======");
+		System.out.println ("1 - Cadastrar Usuário\n" + "2 - Excluir Usuario\n" + "3 - Listar Usuarios\n" + "4 - Sair");
+		System.out.println ("===============================");
+	}
 
 }
