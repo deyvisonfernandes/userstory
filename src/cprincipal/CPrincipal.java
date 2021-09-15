1829d0adad85a05f3ae995d0af58e5f8911dcdc9
Deyvison Fernandes <deyvisonotaviano@gmail.com>
2021-09-15 15:27:31 -0300

    Versao simples de adicionamento de 1 contato

--- src/cprincipal/CPrincipal.java
+++ src/cprincipal/CPrincipal.java
@@ -1,12 +1,26 @@
 package cprincipal;
+
+import java.util.Scanner;
+
 public class CPrincipal {
 	public static void main (String[] args) {
+		Scanner input = new Scanner (System.in);
 		System.out.println ("=======Menu de Interacao=======");
 		System.out.println ("1 - Cadastrar Usuário\n" + "2 - Excluir Usuario\n" + "3 - Listar Usuarios\n" + "4 - Sair");
 		System.out.println ("===============================");
-		Usuario teste1 = new Usuario();
-		teste1.InserirNome();
+		double y;
+		System.out.println ("Digite sua escolha: ");
+		y = input.nextDouble();
+		Usuario teste1 ;
+		teste1 = new Usuario(y);
+		
 		System.out.println ("Nome do Usuario: " + teste1.NomeCompleto);
+		System.out.println ("Email: " + teste1.email);
+		System.out.println ("Perfil do Usuario: " + teste1.perfil);
+		System.out.println ("Senha do Usuario: " + teste1.senha);
+		System.out.println ("Status do Usuario: " + (teste1.ativo==true ? "Ativado" : "Desativado"));
+		
+		
 	}
 
 }
--- src/cprincipal/Usuario.java
+++ src/cprincipal/Usuario.java
@@ -1,5 +1,5 @@
 package cprincipal;
-
+import java.util.Scanner;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -7,18 +7,27 @@ public class Usuario {
 	int id = 000;
 	String NomeCompleto;
 	Date datadenascimento;
-	long senha;
+	String senha;
 	String email;
 	boolean ativo;
 	String perfil;
 	
 	
-	public void InserirNome() {
+	public Usuario(double x) {
 		Scanner input = new Scanner(System.in);
 		System.out.println ("Digite o nome do Usuário: ");
 		NomeCompleto = input.nextLine();
+		System.out.println ("Digite o email do Usuário: ");
+		email = input.nextLine();
+		System.out.println ("Digite o perfil do Usuário (Administrador/Cliente/Fornecedor): ");
+		perfil = input.nextLine();
+		System.out.println ("Digite a senha do Usuário: ");
+		senha = input.nextLine();
+		ativo = true;
+		
 		
 	}
+	
 	
 	
 
