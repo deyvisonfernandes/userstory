20a1271040adc92e4810b862347c39a0ba16456a
Deyvison Fernandes <deyvisonotaviano@gmail.com>
2021-09-21 15:47:53 -0300

    Criacao do sistema de escolha

--- src/cprincipal/CPrincipal.java
+++ src/cprincipal/CPrincipal.java
@@ -7,36 +7,35 @@ import java.util.Scanner;
 public class CPrincipal {
 	public static void main (String[] args) {
 	{
-		 
+		boolean condicao = true; 
+		int escolha;
 		Scanner input = new Scanner (System.in);
-		System.out.println ("=======Menu de Interacao=======");
-		System.out.println ("1 - Cadastrar Usuário");
-		System.out.println ("2 - Excluir Usuario");
-		System.out.println ("3 - Listar Usuarios");
-		System.out.println ("4 - Sair");
-		System.out.println ("===============================");
-		System.out.println ("Digite sua escolha: ");
-		y = input.nextDouble();
-		Usuario teste1 ;
-		teste1 = new Usuario(y);
-		Scanner input = new Scanner(System.in);
-		/*Date nascimento = new Date ();
-		System.out.println ("Digite o nome do Usuário: ");
-		NomeCompleto = input.nextLine();
-		System.out.println ("Digite o email do Usuário: ");
-		email = input.nextLine();
-		System.out.println ("Digite o perfil do Usuário (Administrador/Cliente/Fornecedor): ");
-		perfil = input.nextLine();
-		System.out.println ("Digite a senha do Usuário: ");
-		senha = input.nextLine();
-		ativo = true;*/
-		
-		/*Para ler Usuario.GetnomeCompleto;*/
-		System.out.println ("Nome do Usuario: " + teste1.NomeCompleto);
-		System.out.println ("Email: " + teste1.email);
-		System.out.println ("Perfil do Usuario: " + teste1.perfil);
-		System.out.println ("Senha do Usuario: " + teste1.senha);
-		System.out.println ("Status do Usuario: " + (teste1.ativo==true ? "Ativado" : "Desativado"));
+		while (condicao == true) {
+			System.out.println ("=======Menu de Interacao=======");
+			System.out.println ("1 - Cadastrar Usuário");
+			System.out.println ("2 - Excluir Usuario");
+			System.out.println ("3 - Listar Usuarios");
+			System.out.println ("4 - Sair");
+			System.out.println ("===============================");
+			System.out.println ("Digite sua escolha: ");
+			escolha = input.nextInt();
+			switch (escolha) {
+			case 1:
+				
+			case 2:
+				
+			case 3:
+				
+			case 4:
+				
+			default:
+				System.out.println ("Opcao Incorreta. Tente novamente.");
+				
+			}
+			
+		}
+			
+		}
 		
 		
 	}
